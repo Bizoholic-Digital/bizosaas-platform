2aa98cec5122e2208e3b86cfb9b9c3cf20258ca6
Bizoholic Dev <dev@bizoholic.com>
2025-12-17 15:42:47 +0000

    chore: Finalize configurations for realignment (Plane, Infra, Lago, Connectors)

--- bizosaas-brain-core/brain-gateway/app/connectors/__init__.py
+++ bizosaas-brain-core/brain-gateway/app/connectors/__init__.py
@@ -1,20 +1,20 @@
 from .base import BaseConnector, ConnectorConfig, ConnectorType, ConnectorStatus
 from .registry import ConnectorRegistry
 from .wordpress import WordPressConnector
-# from .zoho_crm import ZohoCRMConnector
-# from .google_analytics import GoogleAnalyticsConnector
-# from .shopify import ShopifyConnector
-# from .google_tag_manager import GoogleTagManagerConnector
-# from .google_ads import GoogleAdsConnector
-# from .facebook import FacebookAdsConnector
-# from .pinterest import PinterestConnector
-# from .whatsapp import WhatsAppConnector
-# from .google_shopping import GoogleShoppingConnector
-# from .snapchat import SnapchatAdsConnector
-# from .telegram import TelegramConnector
+from .zoho_crm import ZohoCRMConnector
+from .google_analytics import GoogleAnalyticsConnector
+from .shopify import ShopifyConnector
+from .google_tag_manager import GoogleTagManagerConnector
+from .google_ads import GoogleAdsConnector
+from .facebook import FacebookAdsConnector
+from .pinterest import PinterestConnector
+from .whatsapp import WhatsAppConnector
+from .google_shopping import GoogleShoppingConnector
+from .snapchat import SnapchatAdsConnector
+from .telegram import TelegramConnector
 from .fluent_crm import FluentCRMConnector
 from .woocommerce import WooCommerceConnector
-# from .trello import TrelloConnector
+from .trello import TrelloConnector
 from .plane import PlaneConnector
 from .lago import LagoConnector
 
@@ -25,20 +25,20 @@ __all__ = [
     "ConnectorStatus",
     "ConnectorRegistry",
     "WordPressConnector",
-    # "ZohoCRMConnector",
-    # "GoogleAnalyticsConnector",
-    # "ShopifyConnector",
-    # "GoogleTagManagerConnector",
-    # "GoogleAdsConnector",
-    # "FacebookAdsConnector",
-    # "PinterestConnector",
-    # "WhatsAppConnector",
-    # "GoogleShoppingConnector",
-    # "SnapchatAdsConnector",
-    # "TelegramConnector",
+    "ZohoCRMConnector",
+    "GoogleAnalyticsConnector",
+    "ShopifyConnector",
+    "GoogleTagManagerConnector",
+    "GoogleAdsConnector",
+    "FacebookAdsConnector",
+    "PinterestConnector",
+    "WhatsAppConnector",
+    "GoogleShoppingConnector",
+    "SnapchatAdsConnector",
+    "TelegramConnector",
     "FluentCRMConnector",
     "WooCommerceConnector",
-    # "TrelloConnector",
+    "TrelloConnector",
     "PlaneConnector",
     "LagoConnector"
 ]
